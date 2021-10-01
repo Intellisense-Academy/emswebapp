73d57a87e1ec59b9f685c56fa645b5b420d33165
Mohammed Rizwan <88297036+Intellisense-Academy@users.noreply.github.com>
2021-10-01 09:47:00 +0530

    Login
    1) Call back-end REST API
    2) Add routes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from "../services/login.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string = "";
+  password: string = "";
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
+
   }
 
+  login() {
+      this.loginService.login(this.username, this.password);
+  }
 }
